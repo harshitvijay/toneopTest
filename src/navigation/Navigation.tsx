183b3276f09b5a0d3359f79e157303e872ff1ccd
harshitvijay <harshitvijay007@gmail.com>
2022-12-19 16:32:29 +0530

    added navigation component

---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,36 @@
+import React, {FC} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from '../Screens/HomeScreen/HomeScreen';
+import UserDetailsScreen from '../Screens/UserDetails/UserDetailsScreen';
+
+export type RootStackParamList = {
+  Home: undefined;
+  UserDetail: {userId: string};
+};
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const Navigation: FC = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#6F00FF',
+          },
+          headerTitle: 'My Application',
+          headerTintColor: '#fff',
+        }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="UserDetail"
+          component={UserDetailsScreen}
+          initialParams={{userId: '1'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
