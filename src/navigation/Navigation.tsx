61a17635f593063c68664af710ef3da1ba9d1682
harshitvijay <harshitvijay007@gmail.com>
2022-12-19 21:28:57 +0530

    added status bar

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -3,33 +3,37 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../Screens/HomeScreen/HomeScreen';
 import UserDetailsScreen from '../Screens/UserDetails/UserDetailsScreen';
+import {StatusBar} from 'react-native';
 
 export type RootStackParamList = {
   Home: undefined;
-  UserDetail: {userId: string};
+  UserDetail: {userId: number};
 };
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const Navigation: FC = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#6F00FF',
-          },
-          headerTitle: 'My Application',
-          headerTintColor: '#fff',
-        }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen
-          name="UserDetail"
-          component={UserDetailsScreen}
-          initialParams={{userId: '1'}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar backgroundColor={'#4400ffca'} />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#6F00FF',
+            },
+            headerTitle: 'My Application',
+            headerTintColor: '#fff',
+          }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen
+            name="UserDetail"
+            component={UserDetailsScreen}
+            initialParams={{userId: 1}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 };
 
